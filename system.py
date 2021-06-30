64250bc9cc575dd4d2b647ee4d42b4d13301c3b1
grekiki <gregor1234567890@gmail.com>
2021-06-30 15:45:30 +0200

    still works

--- system.py
+++ system.py
@@ -1,3 +1,5 @@
+import torch
+from torch import nn
 
 def clamp(x, lo, hi):
     if x < lo:
@@ -13,7 +15,7 @@ class x_controller:
         self.x_ddot = 0
 
         self.dt = 1/100
-        self.rateLimit = 1/10
+        self.rateLimit = 1/100
 
     def step(self, a):
         self.x_ddot, _ = clamp(a, self.x_ddot - self.rateLimit, self.x_ddot + self.rateLimit)
@@ -21,3 +23,35 @@ class x_controller:
         self.x += self.dt * self.x_dot
         
         self.x, _ = clamp(self.x, -1 , 1)
+
+
+def interp(x, xp, fp):
+  N = len(xp)
+
+  def get_interp(xv):
+    hi = 0
+    while hi < N and xv > xp[hi]:
+      hi += 1
+    low = hi - 1
+    return fp[-1] if hi == N and xv > xp[low] else (
+      fp[0] if hi == 0 else
+      (xv - xp[low]) * (fp[hi] - fp[low]) / (xp[hi] - xp[low]) + fp[low])
+
+  return [get_interp(v) for v in x] if hasattr(x, '__iter__') else get_interp(x)
+
+class NeuralNetwork(nn.Module):
+    def __init__(self):
+        super(NeuralNetwork, self).__init__()
+        self.linear_relu_stack = nn.Sequential(
+            nn.Linear(200+300, 64),
+            nn.ReLU(),
+            nn.Linear(64, 64),
+            nn.ReLU(),
+            nn.Linear(64, 32),
+            nn.ReLU(),
+            nn.Linear(32, 1),
+            nn.Tanh()
+        )
+
+    def forward(self, x):
+        return self.linear_relu_stack(x)
